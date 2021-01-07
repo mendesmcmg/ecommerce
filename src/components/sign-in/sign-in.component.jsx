01608fc031e6039ad76514ce7682c070edb8a2fc
Clara Mendes <claraplanejamento@gmail.com>
2021-01-06 23:59:06 -0300

    :sparkles: adds signin component

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,54 @@
+import { Component } from 'react';
+import './sign-in.styles.scss'
+import { Component } from "react";
+
+class SignIn extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            email: ''
+            password: ''
+        }
+    }
+
+    handleSubmit = () => {
+        event.preventDefault()
+        this.setState({email: '', password: ''})
+    }
+
+    handleChange = event => {
+        const { value, name } = event
+    }
+
+    render() {
+        return(
+            <div className='sign-in'>
+                <h2>I already have an account</h2>
+                <span>Sign in with your email and password</span>
+
+                <form onSubmit={this.handleSubmit}>
+                    <input 
+                        name='email'
+                        type='email' 
+                        value={this.state.email} 
+                        onChange={this.handleChange}
+                        required 
+                    />
+                    <label>Email</label>
+                    <input 
+                        name='password' 
+                        type='password' 
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                    />
+                    <label>Password</label>
+
+                    <input type='submit' value='Submit Form'/>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SignIn
