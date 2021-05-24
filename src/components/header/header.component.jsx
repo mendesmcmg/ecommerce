30b29cdefc1f05dac879dbf7b1950f6a544579a1
Clara Mendes <claraplanejamento@gmail.com>
2021-05-24 11:13:40 -0300

    :adhesive_bandage: fixes import typo for user types

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,7 +1,6 @@
 import './header.styles.scss'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
-
 import { auth } from '../../firebase/firebase.utils'
 
 import { ReactComponent as Logo } from '../../assets/crown.svg'
