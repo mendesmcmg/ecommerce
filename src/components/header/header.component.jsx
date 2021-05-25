96338393384c59a59665a5b1076cf23db5ab2bfa
Clara Mendes <claraplanejamento@gmail.com>
2021-05-25 11:50:18 -0300

    :lipstick: adds cart icon component styles

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 import { auth } from '../../firebase/firebase.utils'
 
 import { ReactComponent as Logo } from '../../assets/crown.svg'
+import CartIcon from '../cart-icon/cart-icon.component'
 
 const Header = ({ currentUser }) => (
     <div className='header'>
@@ -28,6 +29,7 @@ const Header = ({ currentUser }) => (
                     SIGN IN
                 </Link>
             }
+            <CartIcon />
         </div>
     </div>
 )
