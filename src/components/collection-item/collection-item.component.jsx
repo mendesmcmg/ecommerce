a987f08652a6cf6a96cf167ed4c5058e0ade2002
Clara Mendes <claraplanejamento@gmail.com>
2021-05-27 16:05:45 -0300

    :lipstick: adds button to add to cart

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,3 +1,4 @@
+import CustomButton from '../custom-button/custom-button.component'
 import './collection-item.styles.scss'
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
@@ -13,6 +14,8 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
         </div>
+
+        <CustomButton inverted>Add to cart</CustomButton>
     </div>
 )
 
--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,11 +1,19 @@
-import './custom-button.styles.scss'
+import "./custom-button.styles.scss";
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps}) => (
-    <button className={`${isGoogleSignIn ? 'google-sign-in': '' } custom-button`} 
+const CustomButton = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) => (
+  <button
+    className={`${inverted ? "inverted" : ""} ${
+      isGoogleSignIn ? "google-sign-in" : ""
+    } custom-button`}
     {...otherProps}
-    >
-        {children}
-    </button>
-)
+  >
+    {children}
+  </button>
+);
 
-export default CustomButton
+export default CustomButton;
